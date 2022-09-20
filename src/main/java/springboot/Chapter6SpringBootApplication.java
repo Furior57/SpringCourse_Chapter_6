9e48bc69a40b6ff13a4258d1852702d0f8fbc6fd
Furior57 <Furior572@gmail.com>
2022-09-20 11:30:54 +0300

    20.09.22_SpringBoot_Creating

---
+++ src/main/java/springboot/Chapter6SpringBootApplication.java
@@ -0,0 +1,24 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Первое, что мы увидим это аннотацию @SpringBootApplication, эта аннотация объединяет в себе
+// три разных аннотации @Configuration, @EnableAutoConfiguration и @ComponentScan
+@SpringBootApplication
+public class Chapter6SpringBootApplication {
+    // Далее мы видим метод run() в который передается main класс, он сам создаст контейнер,
+    // инициализирует бины и конфигурацию. Какой пакет будет сканироваться для поиска этих
+    // компонентов? А сканироваться будет тот пакет где лежит main-класс, как и все его подпакеты.
+    // Этот путь может быть некорректен, но мы можем в аннотации @SpringBootApplication
+    // прописать необходимый пакет.
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter6SpringBootApplication.class,
+                args);
+    }
+    // А теперь рассмотрим вариант создания SpringBoot проекта для бедных :).
+    // Мы можем пользоваться бесплатной IDE, но там такого удобного меню при создании
+    // проекта не будет. Для того чтобы создать подобный проект существует
+    // сайт start.spring.io. Там все элементарно, здесь описывать ничего не будем. Генерируем
+    // проект, скачиваем и открываем нашей IDE./
+
+}
---
+++ src/test/java/springboot/Chapter6SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter6SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
